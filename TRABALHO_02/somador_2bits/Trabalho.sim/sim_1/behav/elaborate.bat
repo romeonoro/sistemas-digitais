0dc53e845691adde51b53debbe206f2a20096e8f
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2025-09-24 10:32:49 -0300

    Delete TRABALHO_02/somador_2bits directory

--- TRABALHO_02/somador_2bits/Trabalho.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-call %xv_path%/xelab  -wto 90585c2eb96c47e78b6763487bef2fe5 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot testbench_behav xil_defaultlib.testbench -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
